4b9277503562da30a9857ab4205c4331b27505a0
diaoxiuchong <17853525629@163.com>
2020-08-14 18:24:17 +0800

    es基本功能

---
+++ src/main/java/com/example/entity/Employee.java
@@ -0,0 +1,24 @@
+package com.example.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+/**
+ * @Author: xiuchong
+ * @Date: 2020/8/14 17:24
+ * @Description: Book实体 加上了@Document注解之后，默认情况下这个实体中所有的属性都会被建立索引、并且分词
+ */
+@Data
+@ToString
+@NoArgsConstructor
+public class Employee {
+
+    private String id;
+    private Long version;
+    String firstName;
+    String lastName;
+    String age;
+    String[] interests;
+
+}
